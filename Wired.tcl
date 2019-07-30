44ed134da5fc4d230b0bf06d552528a6b5a104b6
paroxayte <tristonchav@gmail.com>
2019-07-30 16:57:03 -0500

    properly link params to opt array. tidy up.

--- Wired.tcl
+++ Wired.tcl
@@ -1,15 +1,11 @@
 # Params
-set opt(chan)		Channel;
 set opt(mac)		Mac/802_3;
 set opt(delay)		1ms;
 set opt(numNodes)	60;
-set opt(time)		100;
+set opt(time)		100s;
 set opt(bw)             10Mb;
 set opt(sz)             1000b;
-set opt(ifq) 		Queue/DropTail;
-set opt(ll)             LL;
-set opt(chan)           Channel;
-set opt(phys)           Phy/WiredPhy;
+set opt(cbrRate)        0.01Mb;
 set opt(trace) 		out.tr;
 set opt(nam)		out.nam;
 set node(0)             0;
@@ -51,16 +47,16 @@ proc gen-topology {} {
         $node(0) shape box
 
         #create links between the nodes
-        $ns simplex-link $node(0) $node(1) 0.3Mb 100ms DropTail
-        $ns simplex-link $node(1) $node(0) 0.3Mb 100ms DropTail
-        set lan [$ns newLan $nodelist 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
+        $ns simplex-link $node(0) $node(1) $opt(bw) $opt(delay) DropTail
+        $ns simplex-link $node(1) $node(0) $opt(bw) $opt(delay) DropTail
+        set lan [$ns newLan $nodelist $opt(bw) $opt(delay) LL Queue/DropTail $opt(mac) Channel]
 
         #Give node position
         $ns simplex-link-op $node(0) $node(1) orient right
         $ns simplex-link-op $node(1) $node(0) orient left
 
         #set queue size of link(n0-n1) to 20
-        $ns queue-limit $node(0) $node(1) 20
+        #$ns queue-limit $node(0) $node(1) 20
 }
 
 proc gen-udp {} {
@@ -77,18 +73,17 @@ proc gen-udp {} {
 }
 
 proc gen-cbr {} {
-        global udp
+        global udp opt
 
         #setup a CBR over UDP connection
         set cbr [new Application/Traffic/CBR]
         $cbr attach-agent $udp
         $cbr set type_ CBR
-        $cbr set packet_size_ 1000
-        $cbr set rate_ 0.01Mb
+        $cbr set packet_size_ $opt(sz)
+        $cbr set rate_ $opt(cbrRate)
         $cbr set random_ false
         return $cbr
 }
-
 
 set ns [new Simulator]
 
@@ -105,11 +100,8 @@ set udp [gen-udp]
 set cbr [gen-cbr]
 
 #scheduling the events
-$ns at 0.1 "$cbr start"
-$ns at 125.5 "$cbr stop"
-global ns
 set time 0.1
 set now [$ns now]
 $ns at 0.1 "$cbr start"
- $ns at 10.0 "finish"
- $ns run
+$ns at $opt(time) "finish"
+$ns run
