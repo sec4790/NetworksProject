0eef9cd9f3f7dfc5439ac0ae117af9de6d744b24
sec4790 <seb4790@gmail.com>
2019-07-31 11:04:12 -0500

    added some simple code to the wireless.tcl

--- Wireless.tcl
+++ Wireless.tcl
@@ -11,4 +11,91 @@ set val(ant)	Antenna/OmniAntenna;
 set val(ifqlen)	50;
 set val(nn)		2; 
 set val(rp)		DSDV;
-:q
+
+#Initialize simulator
+set ns_ [new Simulator]
+
+#Initialize trace file
+set tracefd [open wireless_trace.tr w]
+$ns_ trace-all $tracefd
+
+#initialize network animator
+set namtrace [open wireless_trace.nam w]
+
+
+#create topology object that keeps track of movements of mobilenodes
+set topo [new Topography]
+#create grid topology
+$topo load_flatgrid 500 500
+
+#Create God object (General Operations Director)
+create-god $val(nn)
+
+#Configure nodes using API and options from engr.iupui.edu
+$ns_ node-config 		 -adhocRouting $val(rp) \
+                         -llType $val(ll) \
+                         -macType $val(mac) \
+                         -ifqType $val(ifq) \
+                         -ifqLen $val(ifqlen) \
+                         -antType $val(ant) \
+                         -propType $val(prop) \
+                         -phyType $val(netif) \
+                         -topoInstance $topo \
+                         -channelType $val(chan) \
+                         -agentTrace ON \
+                         -routerTrace ON \
+                         -macTrace OFF \
+                         -movementTrace OFF;
+						 
+#Create mobile nodes
+for {set i 0} {$i < $val(nn) } {incr i} {
+               set node_($i) [$ns_ node ];
+               $node_($i) random-motion 0       ;# disable random motion
+       }  
+	   
+	   
+#
+# Provide initial (X,Y, for now Z=0) co-ordinates for node_(0) and node_(1)
+#
+$node_(0) set X_ 5.0
+$node_(0) set Y_ 2.0
+$node_(0) set Z_ 0.0
+
+$node_(1) set X_ 390.0
+$node_(1) set Y_ 385.0
+$node_(1) set Z_ 0.0
+
+#Produce simple node movements where node 1 moves towards node 0#
+$ns_ at 50.0 "$node_(1) setdest 25.0 20.0 15.0"
+$ns_ at 10.0 "$node_(0) setdest 20.0 18.0 1.0"
+$ns_ at 100.0 "$node_(1) setdest 490.0 480.0 15.0"
+
+#Set up connection and traffic flow between the two nodes#
+
+
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+set sink [new Agent/TCPSink]
+$ns_ attach-agent $node_(0) $tcp
+$ns_ attach-agent $node_(1) $sink
+$ns_ connect $tcp $sink
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ns_ at 10.0 "$ftp start" 
+
+#
+# Tell nodes when the simulation ends
+#
+for {set i 0} {$i < $val(nn) } {incr i} {
+    $ns_ at 150.0 "$node_($i) reset";
+}
+$ns_ at 150.0 "stop"
+$ns_ at 150.01 "puts \"NS EXITING...\" ; $ns_ halt"
+proc stop {} {
+    global ns_ tracefd
+    $ns_ flush-trace
+    close $tracefd
+}
+
+puts "Starting Simulation..."
+$ns_ run
