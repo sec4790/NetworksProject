cbfbfd0da6593b18bf44d1cb44edbb88a3a3118a
paroxayte <tristonchav@gmail.com>
2019-08-02 07:00:13 -0500

    final draft

--- Wired.tcl
+++ Wired.tcl
@@ -1,13 +1,19 @@
 # Params
 set opt(mac)		Mac/802_3;
-set opt(delay)		1ms;
-set opt(numNodes)	5;
-set opt(time)		100s;
-set opt(bw)             10Mb;
+set opt(delay)		10ms;
+set opt(numNodes)	60.0;
+set opt(time)		100;
+set opt(bw)             1Mb;
 set opt(sz)             1000b;
 set opt(cbrRate)        0.01Mb;
 set opt(trace) 		out.tr;
 set opt(nam)		out.nam;
+
+# Global state
+set ns                  0;
+set node(0)             0;
+set sink(0)             0;
+set udp(0)              0;
 set cbr(0)              0;
 
 proc gen-trace {} {
@@ -34,32 +40,51 @@ proc finish {} {
         exit 0
 }
 
+proc get-simulator {} {
+        set ns [new Simulator]
+        $ns color 1 Red
+        return $ns        
+}
+
 # Returns stringify'd list on initialized nodes
 proc init-nodes {} {
-	global ns opt node
+	global ns opt node sink udp
 
 	for {set i 0} {$i < $opt(numNodes)} {incr i} {
 		set node($i) [$ns node]
-		lappend nodelist $node($i)
+                set sink($i) [make-sink $node($i)]
+                set udp($i) [make-udp $node($i)]
 	}
-        return $nodelist
+
+        set last [expr int($opt(numNodes) - 1)]
+        $ns duplex-link $node($last) $node(0) $opt(bw) $opt(delay) DropTail
+	for {set i 0} {$i < $last} {incr i} {
+
+                $ns duplex-link $node($i) $node([expr $i + 1]) $opt(bw) $opt(delay) DropTail
+	}
 }
 
-proc gen-udp {sender node} {
-        global ns opt
+# Returns sink handle
+proc make-sink {node} {
+        global ns 
+        
+        set sink [new Agent/Null] 
+        $ns attach-agent $node $sink
+        return $sink
+}
+
+# Returns udp handle
+proc make-udp {node} {
+        global ns
 
         set udp [new Agent/UDP]
-        $ns attach-agent $sender $udp
-        set null [new Agent/Null]
-        set last [expr $opt(numNodes) - 1]
-        $ns attach-agent $node $null
-        $ns connect $udp $null
         $udp set fid_ 1
+        $ns attach-agent $node $udp
         return $udp
 }
 
-proc gen-cbr { udp } {
-        global opt
+proc make-cbr {udp} {
+        global ns opt
 
         set cbr [new Application/Traffic/CBR]
         $cbr attach-agent $udp
@@ -70,76 +95,57 @@ proc gen-cbr { udp } {
         return $cbr
 }
 
+proc rand_range {lo hi} { return [expr int(rand()*($hi-$lo+1)) + $lo] }
 
-proc gen-router-topology {} {
- global node opt cbr ns bnode
+proc connect-pair {numCbr} {
+        global node opt udp sink ns cbr
 
-        # Create broadcasting nodes
-        set bnode(0) [$ns node]
-        set bnode(1) [$ns node]
-        set bnode(2) [$ns node]
-        set bnode(3) [$ns node]
-        set bnode(4) [$ns node]
+        set lIndex $numCbr
+        set rIndex [rand_range 0 [expr $opt(numNodes) -1]]
 
-        set router [$ns node]
-
-        # Create broadcast topology
-        $ns simplex-link $bnode(0) $router $opt(bw) $opt(delay) DropTail
-        $ns simplex-link $bnode(1) $router $opt(bw) $opt(delay) DropTail
-        $ns simplex-link $bnode(2) $router $opt(bw) $opt(delay) DropTail
-        $ns simplex-link $bnode(3) $router $opt(bw) $opt(delay) DropTail
-        $ns simplex-link $bnode(4) $router $opt(bw) $opt(delay) DropTail
-
-
-        $ns simplex-link-op $bnode(0) $router orient down
-        $ns simplex-link-op $bnode(1) $router orient down-right
-        $ns simplex-link-op $bnode(2) $router orient right
-        $ns simplex-link-op $bnode(3) $router orient up-right
-        $ns simplex-link-op $bnode(4) $router orient up
-
-        set last [expr $opt(numNodes) - 1]
-        set udp [gen-udp $router $node($last)]
-        set cbr(0) [gen-cbr $udp]
-        set udp [gen-udp $bnode(1) $node($last)]
-        set cbr(1) [gen-cbr $udp]
-        set udp [gen-udp $bnode(2) $node($last)]
-        set cbr(2) [gen-cbr $udp]
-        set udp [gen-udp $bnode(3) $node($last)]
-        set cbr(3) [gen-cbr $udp]
-        set udp [gen-udp $bnode(4) $node($last)]
-        set cbr(4) [gen-cbr $udp]
-
-        return $router
+        if {$lIndex == $rIndex} {
+                connect-pair $numCbr        
+        } else {
+                set sndr $udp($lIndex)
+                set cbr($numCbr) [make-cbr $sndr] 
+                $node($rIndex) color Red
+                set rcvr $sink($rIndex)
+                $ns connect $sndr $rcvr
+        }
+}
+ 
+proc connect-pairs {n} {
+	for {set i 0} {$i < $n} {incr i} {
+                connect-pair $i
+        }
 }
 
-set ns [new Simulator]
+proc gen-events {n} {
+        global opt ns cbr
+
+        $ns at 0.1 "$cbr(0) start"; # Doesn't like starting at 0
+        set delta [expr $opt(time) / $n]
+	for {set i 1} {$i < $n} {incr i} {
+                set ts [expr $delta * $i]
+                $ns at $ts "$cbr($i) start"
+        }
+}
+
+set ns [get-simulator]
 
 set trfd [gen-trace]
 set namfd [gen-namtrace]
 
-#define color for data flows
-$ns color 1 Red
 #create six nodes
 set nodelist [init-nodes]
-set router [gen-router-topology]
-
-$router color Red
-$router shape circle
 
 #create links between the nodes
-$ns duplex-link $router $node(0) 2Mb 10ms DropTail
-set lan [$ns newLan $nodelist 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
-
-#Give node position
-$ns duplex-link-op $router $node(0) orient right
-
+connect-pairs $opt(numNodes)
 
 #scheduling the events
-$ns at 0.1 "$cbr(0) start"
-$ns at 10.0 "$cbr(0) stop"
+gen-events $opt(numNodes)
 global ns
 set time 0.1
 set now [$ns now]
-$ns at 10.0 "finish"
+$ns at $opt(time) "finish"
 $ns run
- 
